c32e207ef4c9e28c7150ed67efe32ed180093f7c
Sahil <balaji.sahil45987@gmail.com>
2024-10-19 08:33:51 +0000

    Data types

---
+++ 02_basics_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treats all js code as newer version
+
+// alert (3 + 3)  //we are using node js not browser
+
+//avoid semicolons
+//CODE SHOULD BE READABLE ---VERY IMPORTANT
+
+console.log(3 + 3)  
+// ; no need just type in below line
+console.log("Sahil")
+
+let name = "sahil" //string
+let age = 18    //integer
+let isloggedin = false
+let state;
+
+
+//number => find range and memory size
+//bigint => larger than number
+//string =>  ""
+//boolean => true/false
+//null => standalone value => means empty(type:null) in JS
+//undefined => value not defined or declared
+//symbol => unique 
+//object => 
+
+//typeof
+console.log(typeof "sahil")  //string
+console.log(typeof null)     //object
+console.log(typeof undefined)    //undefined
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavascriptLearning
 Revising JS in detail for open source GSoC 2025 prep.
+node filname.js  => command to run JS in console
